410c7bdaa6cb9bdd2a6876a71fb1781d220d163d
chaudhuree <chaudhuree00@gmail.com>
2022-03-23 01:43:46 +0600

    docs: added code for mock data and display them to the website through info component

--- src/context/context.js
+++ src/context/context.js
@@ -1,15 +1,31 @@
-import React, { useState, useEffect, useContext } from "react";
-import mockUser from "./mockData.js/mockUser";
-import mockRepos from "./mockData.js/mockRepos";
+import React, { useContext, useState } from "react";
 import mockFollowers from "./mockData.js/mockFollowers";
-import axios from "axios";
+import mockRepos from "./mockData.js/mockRepos";
+import mockUser from "./mockData.js/mockUser";
 
 const rootUrl = "https://api.github.com";
 
 const GithubContext = React.createContext();
 export const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  // request loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  // error
+  const [error, setError] = useState({ show: false, msg: '' });
+
   return (
-      <GithubContext.Provider value={"hello"}>{children}</GithubContext.Provider>
+      <GithubContext.Provider value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        // searchGithubUser,
+        isLoading,
+      }}>{children}</GithubContext.Provider>
   );
 };
 
